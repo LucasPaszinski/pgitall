c20fc05b83a730f7086885690e07f28df1e587df
lucaspaszinski <lucaspaszinski@gmail.com>
2020-05-17 09:34:33 -0300

    git all is reborn, now in parallel.

---
+++ lib/gitall/gitall.ex
@@ -0,0 +1,43 @@
+defmodule GitAll do
+  #implementar o a escolha do comando disparado para os repositórios
+  def start(path, command) do
+    sub_dirs = get_git_sub_dirs(path)
+    #dispatch messages
+    for dir <- sub_dirs do
+      me = self()
+      spawn(fn ->
+        exec_on = Path.join(path, dir)
+        result = System.cmd("git", ["status"], cd: exec_on )
+        send me, {:git, dir, result}
+      end)
+    end
+    #receive then back
+    for _dir <- sub_dirs do
+      receive do
+        {:git, header,{text, 0}} ->
+          IO.puts header
+          IO.puts text
+      end
+    end
+  end
+
+  def get_git_sub_dirs(path) do
+    File.ls!(path)
+    |> Enum.filter(&File.dir?(Path.join(path,&1)))
+    |> Enum.filter(fn sub_dir ->
+                    File.ls!(Path.join(path,sub_dir))
+                    |> Enum.any?(&(&1 == ".git")) end)
+
+  end
+
+  def get_returns(n, res_to, msgs \\ []) do
+    if n > 0 do
+      msgs
+    end
+
+    receive do
+      {:msg, value} ->
+        get_returns(n-1, res_to,  [msgs | value])
+    end
+  end
+end
